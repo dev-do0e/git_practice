10a4be5c06afcfbeb491b898bc6ad20eaec09baf
dev-do0e <dyu8563@gmail.com>
2025-12-08 13:25:07 +0900

    Exception folder 'build'

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/workspace/Developments/build/install_local_manifest.txt"
+  file(WRITE "D:/workspace/Developments_git_practice/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/workspace/Developments/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/workspace/Developments_git_practice/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
